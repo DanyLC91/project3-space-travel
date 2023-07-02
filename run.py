316056209dd47e12027efb4e50f056b7b2742e4d
DanyLC91 <daniela.campisi@hotmail.com>
2023-07-02 22:27:12 +0000

    Adding credentials and importing google auth

---
+++ run.py
@@ -0,0 +1,16 @@
+#All the imports for the program
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+#Program credentials and setting up link for spreadsheet
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('space_travel_data')
