780bd653a0c0502b15c90e0edeeb05761d5a253e
DanyLC91 <daniela.campisi@hotmail.com>
2023-07-02 22:34:20 +0000

    start interface consisting in welcome message and a main menu with 6 options with user input

--- run.py
+++ run.py
@@ -14,3 +14,28 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('space_travel_data')
+
+#Welcome message
+print('''
+ 
+                      \033[1m CosmoLink Travel \033[0m
+        
+                'Where Imagination Meets Infinity'
+
+
+''')
+
+#Main Menu setting 
+def menu():
+    print("Menu:")
+    print("1. Bio")
+    print("2. Our Packages")
+    print("3. FAQs")
+    print("4. Get your Ticket")
+    print("5. Contact")
+    print("6. Exit")
+
+    choice = input("To navigate our Menu, Please one of the following\
+options (1-6):\n ")
+
+
