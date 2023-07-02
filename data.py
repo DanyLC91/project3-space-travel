ba64ced21fbf293332d02454eb33f0dbb4dc48d8
DanyLC91 <daniela.campisi@hotmail.com>
2023-07-02 22:45:55 +0000

    New Python file created to storefunctions with big pieces of text that will be called back on the main file menu options

---
+++ data.py
@@ -0,0 +1,64 @@
+#Function to host BIO data
+def bio():
+    print('''
+                                        \033[1m About Us \033[0m
+
+
+
+         ''')
+    print('''
+        Introducing CosmoLink Travel, an innovative space travel agency \
+founded in 2020,
+        with a bold vision to unlock the wonders of our solar system \
+and make interplanetary
+        exploration a reality. Based in the heart of the European Union,\
+CosmoLink Travel 
+        has quickly established itself as a premier provider of extraordinary \
+journeys to the
+        planets of our celestial neighborhood.
+
+        As pioneers in the field of commercial space travel, CosmoLink Travel \
+has brought together
+        a team of brilliant engineers, experienced pilots, and passionate \
+space enthusiasts to
+        deliver an unparalleled experience for our clients. Our mission is \
+to bridge the gap 
+        between science fiction and reality by offering safe and exhilarating \
+flights around the 
+        planets of our solar system.
+
+        CosmoLink Travel understands that space travel is not just about the \
+destination,
+        but also the journey itself. We have meticulously designed our \
+spacecraft to ensure 
+        optimal comfort, safety, and awe-inspiring views of the cosmic \
+wonders that surround us. 
+        Our fleet of state-of-the-art interplanetary shuttles boasts \
+cutting-edge technology, 
+        top-notch navigation systems, and luxurious accommodations to ensure \
+an unforgettable 
+        experience for every traveler.
+
+        While our headquarters are based in the European Union, we have \
+strategically chosen launch
+        sites in the United States to take advantage of the advanced \
+infrastructure and expertise 
+        available there. Our close collaboration with renowned American \
+aerospace partners enables us
+        to provide the highest standards of safety and reliability for our \
+interplanetary flights.
+
+        At CosmoLink Travel, we believe that space travel should be \
+accessible to all those with a 
+        thirst for adventure and a curiosity to explore the unknown. Whether \
+you dream of gazing at the
+        majestic rings of Saturn, walking on the surface of Mars, or \
+marveling at the swirling storms of
+        Jupiter, CosmoLink Travel is your gateway to a universe of \
+possibilities.
+
+        Join us on a cosmic journey of discovery with CosmoLink Travel, \
+where the boundaries of 
+        imagination are transcended, and the wonders of the solar system 
+are within your reach.
+    ''')
