e8d7cfdf0536dd2e50385ea2fc83b59077cf38a1
DanyLC91 <daniela.campisi@hotmail.com>
2023-07-03 08:23:07 +0000

    80 character deployment fix

--- data.py
+++ data.py
@@ -1,66 +1,50 @@
 #Function to host BIO data
 def bio():
     print('''
-                                        \033[1m About Us \033[0m
+                 \033[1m About Us \033[0m
 
 
 
          ''')
     print('''
-        Introducing CosmoLink Travel, an innovative space travel agency \
-founded in 2020,
-        with a bold vision to unlock the wonders of our solar system \
-and make interplanetary
-        exploration a reality. Based in the heart of the European Union,\
-CosmoLink Travel 
-        has quickly established itself as a premier provider of extraordinary \
-journeys to the
-        planets of our celestial neighborhood.
+Introducing CosmoLink Travel, an innovative space travel agency 
+founded in 2020, with a bold vision to unlock the wonders of our solar 
+system and make interplanetary exploration a reality. Based in the heart 
+of the European Union, CosmoLink Travel has quickly established itself 
+as a premier provider of extraordinary journeys to the planets of our 
+celestial neighborhood.
 
-        As pioneers in the field of commercial space travel, CosmoLink Travel \
-has brought together
-        a team of brilliant engineers, experienced pilots, and passionate \
-space enthusiasts to
-        deliver an unparalleled experience for our clients. Our mission is \
-to bridge the gap 
-        between science fiction and reality by offering safe and exhilarating \
-flights around the 
-        planets of our solar system.
+As pioneers in the field of commercial space travel, CosmoLink Travel 
+has brought together a team of brilliant engineers, experienced pilots, 
+and passionate space enthusiasts to deliver an unparalleled experience 
+for our clients. Our mission is to bridge the gap between science 
+fiction and reality by offering safe and exhilarating flights around 
+the planets of our solar system.
 
-        CosmoLink Travel understands that space travel is not just about the \
-destination,
-        but also the journey itself. We have meticulously designed our \
-spacecraft to ensure 
-        optimal comfort, safety, and awe-inspiring views of the cosmic \
-wonders that surround us. 
-        Our fleet of state-of-the-art interplanetary shuttles boasts \
-cutting-edge technology, 
-        top-notch navigation systems, and luxurious accommodations to ensure \
-an unforgettable 
-        experience for every traveler.
+ CosmoLink Travel understands that space travel is not just about the 
+destination, but also the journey itself. We have meticulously designed 
+our spacecraft to ensure optimal comfort, safety, and awe-inspiring 
+views of the cosmic wonders that surround us. Our fleet of state-of-the-art 
+interplanetary shuttles boasts cutting-edge technology, top-notch 
+navigation systems, and luxurious accommodations to ensure an unforgettable 
+experience for every traveler.
 
-        While our headquarters are based in the European Union, we have \
-strategically chosen launch
-        sites in the United States to take advantage of the advanced \
-infrastructure and expertise 
-        available there. Our close collaboration with renowned American \
-aerospace partners enables us
-        to provide the highest standards of safety and reliability for our \
+While our headquarters are based in the European Union, we have 
+strategically chosen launch sites in the United States to take advantage 
+of the advanced infrastructure and expertise available there. Our close 
+collaboration with renowned American aerospace partners enables us to 
+provide the highest standards of safety and reliability for our 
 interplanetary flights.
 
-        At CosmoLink Travel, we believe that space travel should be \
-accessible to all those with a 
-        thirst for adventure and a curiosity to explore the unknown. Whether \
-you dream of gazing at the
-        majestic rings of Saturn, walking on the surface of Mars, or \
-marveling at the swirling storms of
-        Jupiter, CosmoLink Travel is your gateway to a universe of \
-possibilities.
+At CosmoLink Travel, we believe that space travel should be accessible 
+to all those with a thirst for adventure and a curiosity to explore 
+the unknown. Whether you dream of gazing at the majestic rings of Saturn, 
+walking on the surface of Mars, or marveling at the swirling storms of
+Jupiter, CosmoLink Travel is your gateway to a universe of possibilities.
 
-        Join us on a cosmic journey of discovery with CosmoLink Travel, \
-where the boundaries of 
-        imagination are transcended, and the wonders of the solar system 
-are within your reach.
+Join us on a cosmic journey of discovery with CosmoLink Travel, where 
+the boundaries of imagination are transcended, and the wonders of the solar
+system are within your reach.
     ''')
 
 #Fuction to host planets trips description
@@ -68,18 +52,14 @@ def mercury_text():
     print('''Trip to Mercury
         
         
-        Experience the scorching beauty of Mercury, the closest planet to \
+Experience the scorching beauty of Mercury, the closest planet to 
 the Sun.
-        This 15-day journey will take you on an incredible adventure as you \
-orbit
-        around Mercury for two days. Witness its rugged terrain, \
-captivating craters,
-        and mesmerizing sunsets. Immerse yourself in the mysteries of this \
-unique 
-        planet while our expert guides provide fascinating insights into \
-its geological
-        wonders. Don't miss the chance to embark on this extraordinary \
-journey for $18,000.
+This 15-day journey will take you on an incredible adventure as you 
+orbit around Mercury for two days. Witness its rugged terrain, 
+captivating craters, and mesmerizing sunsets. Immerse yourself in the
+mysteries of this unique planet while our expert guides provide fascinating
+insights into its geological wonders. Don't miss the chance to embark on 
+this extraordinary journey for $18,000.
         ''')
 
 
@@ -87,17 +67,12 @@ def venus_text():
     print('''Trip to Venus
         
         
-        Embark on a 20-day expedition to the enchanting world of Venus. /
-Discover 
-        the planet's thick atmosphere, swirling clouds, and volcanic /
-landscapes during
-        a three-day orbit around Venus. Marvel at its intense beauty and /
-learn about its 
-        intriguing climate and geological features from our knowledgeable /
-guides. This 
-        exclusive package is available for $22,500, offering an /
-unforgettable Venusian 
-        experience.
+Embark on a 20-day expedition to the enchanting world of Venus. 
+Discover the planet's thick atmosphere, swirling clouds, and volcanic 
+landscapes during a three-day orbit around Venus. Marvel at its intense 
+beauty and learn about its intriguing climate and geological features from 
+our knowledgeable guides. This exclusive package is available for $22,500,
+offering an unforgettable Venusian experience.
         ''')
 
 
@@ -105,17 +80,12 @@ def the_moon_text():
     print('''Trip to the Moon
         
         
-        Experience the closest celestial neighbor like never before with /
-our 7-day Moon 
-        Adventure Package. Witness the magnificence of Earth's Moon during /
-a one-day 
-        orbit, observing its majestic craters, lunar landscapes, and the /
-Earth-rise from 
-        a unique vantage point. Delve into the history of lunar exploration /
-and enjoy the 
-        tranquility of space. This lunar escapade is priced at $12,000, /
-providing an 
-        incredible journey to our celestial companion.
+Experience the closest celestial neighbor like never before with our 7-day
+Moon Adventure Package. Witness the magnificence of Earth's Moon during a 
+one-day orbit, observing its majestic craters, lunar landscapes, and the
+Earth-rise from a unique vantage point. Delve into the history of lunar
+ exploration and enjoy the tranquility of space. This lunar escapade is 
+priced at $12,000, providing an incredible journey to our celestial companion.
         ''')
 
 
@@ -123,17 +93,13 @@ def mars_text():
     print('''Trip to Mars
         
         
-        Embark on the ultimate Martian adventure with our 90-day Mars \
-Expedition Package. 
-        Experience the Red Planet like a true explorer during a \
-seven-day orbit 
-        around Mars. Marvel at its rusty terrain, towering volcanoes,\
-and deep canyons. 
-        Engage in immersive activities like rover excursions and geological\
-explorations 
-        led by our expert team. This extraordinary journey to Mars is\
-available for 
-        $75,000, offering an unparalleled interplanetary experience.
+Embark on the ultimate Martian adventure with our 90-day Mars Expedition 
+Package. 
+Experience the Red Planet like a true explorer during a seven-day orbit 
+around Mars. Marvel at its rusty terrain, towering volcanoes,and deep canyons. 
+Engage in immersive activities like rover excursions and geological 
+explorations led by our expert team. This extraordinary journey to Mars is
+available for $75,000, offering an unparalleled interplanetary experience.
         ''')
 
 
@@ -141,17 +107,13 @@ def jupyter_text():
     print('''Trip to Jupiter
         
         
-        Unveil the secrets of the gas giant with our 120-day Jupiter /
-Discovery Package. 
-        Orbit around Jupiter for an exhilarating 14 days and witness its /
-iconic bands, 
-        colossal storms, and mesmerizing auroras. Immerse yourself in the /
-wonders of this 
-        giant planet, while our knowledgeable guides provide captivating /
-insights into 
-        its complex atmosphere and unique features. This exclusive journey /
-to Jupiter is 
-        priced at $120,000, promising an unforgettable cosmic experience.
+Unveil the secrets of the gas giant with our 120-day Jupiter Discovery Package. 
+Orbit around Jupiter for an exhilarating 14 days and witness its iconic bands, 
+colossal storms, and mesmerizing auroras. Immerse yourself in the wonders 
+of this giant planet, while our knowledgeable guides provide captivating 
+insights into its complex atmosphere and unique features. This exclusive 
+journey to Jupiter is priced at $120,000, promising an unforgettable cosmic
+experience.
         ''')
 
 
@@ -159,17 +121,13 @@ def saturn_text():
     print('''Trip to Saturn
         
         
-        Embark on a mesmerizing 150-day journey to the majestic planet /
-Saturn. Experience 
-        the awe-inspiring beauty of its iconic rings during a 21-day orbit /
+Embark on a mesmerizing 150-day journey to the majestic planet Saturn. 
+Experience the awe-inspiring beauty of its iconic rings during a 21-day orbit 
 around Saturn. 
-        Marvel at the intricate ring structure, explore its numerous moons, /
-and witness 
-        the planet's unique atmospheric features. Our expert guides will /
-provide captivating 
-        commentary, enriching your understanding of this gas giant. Don't /
-miss this 
-        extraordinary adventure, priced at $150,000.
+Marvel at the intricate ring structure, explore its numerous moons, and witness 
+the planet's unique atmospheric features. Our expert guides will provide 
+captivating commentary, enriching your understanding of this gas giant. Don't 
+miss this extraordinary adventure, priced at $150,000.
         ''')
 
 
@@ -177,17 +135,12 @@ def uranus_text():
     print('''Trip to Uranus
         
         
-        Embark on a once-in-a-lifetime 200-day expedition to the /
-mysterious planet Uranus. 
-        Experience the awe-inspiring blue-green atmosphere and witness its /
-unique sideways 
-        rotation during a 30-day orbit around Uranus. Explore its icy moons, /
-observe its complex 
-        weather patterns, and delve into the wonders of this distant giant. /
-Our experienced 
-        guides will accompany you on this extraordinary journey. The Uranus /
-Odyssey Package 
-        is priced at $180,000.
+Embark on a once-in-a-lifetime 200-day expedition to the mysterious planet
+Uranus.  Experience the awe-inspiring blue-green atmosphere and witness its 
+unique sideways rotation during a 30-day orbit around Uranus. Explore its 
+icy moons, observe its complex weather patterns, and delve into the wonders
+of this distant giant. Our experienced guides will accompany you on this 
+extraordinary journey. The Uranus Odyssey Package is priced at $180,000.
         ''')
 
 
@@ -195,17 +148,13 @@ def neptune_text():
     print('''Trip to Neptune
         
         
-        Dive into the depths of the icy blue planet Neptune with our /
-exclusive 250-day 
-        Deep Dive Package. Orbit around Neptune for 40 days and immerse /
-yourself in its 
-        captivating atmosphere, swirling storms, and stunning cloud /
-formations. Witness the distant 
-        beauty of its moons and gain insights into the mysteries of this /
-outermost planet of 
-        the solar system. The Neptune Deep Dive Package is available /
-for $200,000, offering a 
-        truly extraordinary interplanetary adventure.
+Dive into the depths of the icy blue planet Neptune with our exclusive 
+250-day Deep Dive Package. Orbit around Neptune for 40 days and immerse 
+yourself in its captivating atmosphere, swirling storms, and stunning cloud
+formations. Witness the distant beauty of its moons and gain insights into 
+the mysteries of this outermost planet of the solar system. The Neptune
+Deep Dive Package is available for $200,000, offering a truly extraordinary
+interplanetary adventure.
         ''')
 
 #Availability fuction
@@ -220,32 +169,32 @@ def faqs():
     print("\033[1mFAQs\033[0m")
     print('''
     1. What type of spacecraft do you use for interplanetary flights?
-    2. Can anyone participate in space travel, or are there any age /
+    2. Can anyone participate in space travel, or are there any age
 or health restrictions?
-    3. How long does it take to travel from Earth to different planets /
+    3. How long does it take to travel from Earth to different planets
 in the solar system?
-    4. What kind of training or preparation do travelers undergo /
+    4. What kind of training or preparation do travelers undergo
 before their space journey?
-    5. Do I need to have any prior experience in aviation or space-related /
+    5. Do I need to have any prior experience in aviation or space-related 
 activities to book a trip?
     6. Can I bring personal belongings or souvenirs from space back to Earth?
     7. How many passengers can your spacecraft accommodate on each trip?
     8. What is the duration of a typical space travel experience?
-    9. What kind of views can I expect during the journey and while /
+    9. What kind of views can I expect during the journey and while 
 orbiting around the planets?
-    10. Are there any onboard facilities or amenities available during /
+    10. Are there any onboard facilities or amenities available during 
 the trip?
     11. What happens if there is an emergency during the space journey?
     12. Can I customize my space travel package or itinerary?
-    13. Is there a weight or size limit for personal belongings carried /
+    13. Is there a weight or size limit for personal belongings carried 
 onboard?
-    14. How do you handle medical emergencies or special dietary /
+    14. How do you handle medical emergencies or special dietary
 requirements during the trip?
-    15. Are there any restrictions on photography or recording devices /
+    15. Are there any restrictions on photography or recording devices 
 during the journey?
-    16. What happens if there is a delay or cancellation of a scheduled /
+    16. What happens if there is a delay or cancellation of a scheduled 
 space travel departure?
-    17. Are there any specific regulations or permits required for /
+    17. Are there any specific regulations or permits required for 
 international travelers?
     18. How safe is space travel with CosmoLink Travel?
     19. How far in advance should I book my space travel experience?
